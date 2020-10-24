7aff46790bf1d7415ceed31472de0cf2861fee49
Daria Drozdova <dariadrozdova10@gmail.com>
2020-10-24 16:55:20 +0300

    feat: add an implementation of morse-decoder task

--- src/index.js
+++ src/index.js
@@ -1,46 +1,75 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const DECODING_TABLE = {
+    '00': '',
+    '10': '.',
+    '11': '-',
 }
 
 module.exports = {
     decode
+};
+
+function decode(expr) {
+    const sentence = [];
+    let pair = [];
+    let letter = [];
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === '*') {
+            sentence.push(' ');
+            i += 9;
+            continue;
+        }
+        if (i % 2 === 0) {
+            pair.push(expr[i]);
+        } else {
+            pair.push(expr[i]);
+            const decodedMorseSymbol = DECODING_TABLE[pair.join('')];
+            letter.push(decodedMorseSymbol);
+            pair = [];
+            if ((i + 1) % 10 === 0) {
+                const decodedSymbol = MORSE_TABLE[letter.join('')];
+                sentence.push(decodedSymbol);
+                letter = [];
+            }
+        }
+    }
+    return sentence.join('');
 }
